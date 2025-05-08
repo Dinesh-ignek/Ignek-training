5f7b513d75dd1cd2df3e81975b6df4e6bebfd555
Dinesh-ignek <Dinesh.kumar@ignek.com>
2025-05-08 10:11:41 +0530

    add build.gradle

--- modules/liferay-model-listerner/src/main/java/com/liferay/model/listener/ModelListenerLiferay.java
+++ modules/liferay-model-listerner/src/main/java/com/liferay/model/listener/ModelListenerLiferay.java
@@ -1,5 +1,6 @@
 package com.liferay.model.listener;
 
+
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -10,12 +11,15 @@ import com.liferay.portal.kernel.model.User;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
-@Component(service = ModelListener.class, immediate = true)
-public class ModelListenerLiferay extends BaseModelListener<User> {
+@Component(
+	    service = ModelListener.class,
+	    immediate = true
+	)
+	public class ModelListenerLiferay extends BaseModelListener<User> {
 
 	@Activate
 	protected void activate() {
-		_log.info("ModelListenerLiferay activated");
+	    _log.info("ModelListenerLiferay activated");
 	}
 
 	public void onBeforeCreate(User userModel) throws ModelListenerException {
@@ -24,15 +28,16 @@ public class ModelListenerLiferay extends BaseModelListener<User> {
 		super.onBeforeCreate(userModel);
 	}
 
+	
 	public void onAfterCreate(User userModel) throws ModelListenerException {
 		_log.info("in onAfterCreate method");
 		_log.info("userModel : " + userModel);
 		super.onAfterCreate(userModel);
 	}
-
-	public void onAfterRemove(User UserModel) throws ModelListenerException {
+	public void onAfterRemove(User UserModel) throws ModelListenerException{
 		_log.info("removed user" + UserModel.getUserId());
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(ModelListenerLiferay.class);
+	
+	private static final Log _log = LogFactoryUtil.getLog(
+			ModelListenerLiferay.class);
 }
